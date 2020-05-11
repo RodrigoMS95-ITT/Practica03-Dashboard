9b6087c9f9fd214509fb46a3774ce16e4e59d507
RodrigoMS95-ITT <rodrigo.murguia17@tectijuana.edu.mx>
2020-05-11 06:30:17 -0700

    p03

---
+++ ControlInventario/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlTypes;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ControlInventario.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductCode { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public SqlMoney Price { get; set; }
+        public int SupplirId { get; set; }
+        [ForeignKey("SupplirId")]
+        public Supplier Supplier { get; set; }
+
+    }
+}
